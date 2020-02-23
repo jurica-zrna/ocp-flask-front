1c0b6349b21ad5e3e2bba2fd62d1b05e1a82f9d0
Jurica Zrna <jurica.zrna@ibm.com>
2020-02-23 15:56:47 +0100

    added port

--- app/main.py
+++ app/main.py
@@ -10,12 +10,13 @@ CORS(app)
 logging.basicConfig(level=logging.INFO)
 
 host = os.getenv('HOSTNAME', 'localhost')
-url = os.getenv('BACKEND_URL', 'localhost')
+backend_url = os.getenv('BACKEND_URL', 'localhost')
+backend_port = os.getenv('BACKEND_PORT', '8080')
 title = ("Flask on %s" % host)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title=title, host=host, url = url, num = 10)
+    return render_template('index.html', title=title, host=host, url = backend_url, port= backend_port, num = 10)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
